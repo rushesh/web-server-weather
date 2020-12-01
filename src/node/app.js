b2ab439ea380cbef34b0fbf1e81d6643bbcedd0d
Rushesh Sharma <sharmarushesh@gmail.com>
2020-12-01 14:50:15 +0530

    More Routes Added

--- src/node/app.js
+++ src/node/app.js
@@ -7,6 +7,7 @@ const geoloc = require('./utils/geolocation')
 const getweather = require('./utils/getweather')
 const getcities = require('./utils/getcities')
 const port = process.env.PORT || 3000
+
 const staticPath = path.join(__dirname,'../../public')
 const hbsViewsPath = path.join(__dirname,'../../templates/views')
 const hbsPartialPath = path.join(__dirname,'../../templates/partials')
@@ -105,6 +106,40 @@ app.get('/weather',(req,res)=>{
             }
     })
 })
+
+
+app.get('/getweather',(req,res)=>{
+    
+        if(!req.query.address){
+            console.log('No location provided. User forgot to mention loc!')
+            return res.send({
+               address:'address not mentioned in query.',
+                error:404,
+                msg:'You forgot to mention the location. Please provide the location you wish to see weather for.'
+            })
+        }
+        geoloc.getloclatlong(req.query.address,(error,data)=>{
+            if(error){
+                return res.send({error:error,msg:error})
+            }else{
+                getweather.getweather(data,(err,wetdata)=>{
+                    if(err){
+                        return res.send({error:err,msg:err})
+                    }else{
+                        const { location,region,country,currenttemperature,currentfeelslike,weather_descriptions,precipitation} = wetdata
+                        const forecast = `Weather is ${weather_descriptions}. The chances of rain are ${precipitation}%.`
+                        return res.send({
+                            forecast:forecast,
+                            location:`${location}, ${region}`
+                        })
+                    }
+                })
+                }
+        })
+    })
+    
+    
+
 app.get('/getcities',(req,res)=>{
     getcities.getcities((data)=>{
         console.log(data.length)
